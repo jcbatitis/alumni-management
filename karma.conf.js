3e41ae0673a1d3af9aba06dff0965c5fd031fffc
Jejomar Batitis <jcbatitis@hotmail.com>
2023-05-04 15:17:34 +1000

    DEVELOP: fixes project name and added environment variables for prod

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project-jcb'),
+      dir: require('path').join(__dirname, './coverage/alumni-management'),
       subdir: '.',
       reporters: [
         { type: 'html' },
