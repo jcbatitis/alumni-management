13ac33fa003220e2c39351c033ec00e348ed5f5f
Jejomar Batitis <jcbatitis@hotmail.com>
2023-05-04 15:15:42 +1000

    DEVELOP: fixes project name and added environment variables for prod

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-jcb'`, () => {
+  it(`should have as title 'alumni-management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-jcb');
+    expect(app.title).toEqual('alumni-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project-jcb app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('alumni-management app is running!');
   });
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyA8momFnQVXu63I0z2nf2iraeKX9Try2Og',
+  baseURL: 'https://us-central1-alumni-api-s3856624.cloudfunctions.net',
+  authSignupURL: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
+  authSigninURL: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
 };
